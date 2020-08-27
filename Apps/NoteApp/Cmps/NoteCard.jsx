b61aae0f0db38214957409bd24986fc7aab68a49
David Marom <davidmarom@hotmail.com>
2020-08-27 20:48:23 +0300

    color control, list progress

--- Apps/NoteApp/Cmps/NoteCard.jsx
+++ Apps/NoteApp/Cmps/NoteCard.jsx
@@ -19,29 +19,41 @@ export class NoteCard extends React.Component {
         this.props.history.push(`/note`);
     }
 
+    removeListItem = (ev) =>{
+        console.log(ev.target.getAttribute("data-id"))
+        var ALLNOTES = this.props.allNotes
+
+        var tempList = ALLNOTES[this.props.currentNoteId].list;
+        var tempList2 = tempList.splice(ev.target.getAttribute("data-id"),1);
+
+        var ALLNOTES = this.props.allNotes
+        ALLNOTES[this.props.currentNoteId].list = tempList2;
+        noteService.updateNotes(ALLNOTES);
+        this.setState({ notes: ALLNOTES });
+        this.props.ln();
+        this.setState({ colorModal: 'hide' })
+        this.props.history.push(`/note`);
+
+    }
+
     openColorSelect = () => {
         if (this.state.colorModal === 'show') { this.setState({ colorModal: 'hide' }) };
         if (this.state.colorModal === 'hide') { this.setState({ colorModal: 'show' }) };
     }
 
     updateColor = (ev) => {
-
         var ALLNOTES = this.props.allNotes
         ALLNOTES[this.props.currentNoteId].bg = ev.target.getAttribute('value');
-
         noteService.updateNotes(ALLNOTES);
         this.setState({ notes: ALLNOTES });
         this.props.ln();
+        this.setState({ colorModal: 'hide' })
         this.props.history.push(`/note`);
-
     }
 
     render() {
         const { note } = this.state;
-
-
         return (
-
             <div className={`note-card ${this.props.content.bg}`}>
                 <div className="upper-part">
                     <h3>{!note && this.props.content.title}</h3>
@@ -49,42 +61,29 @@ export class NoteCard extends React.Component {
                     <img src={`${this.props.content.url}`}></img>
 
                     {this.props.content.type === 'list' ?
-
                         this.props.content.list.map((item, idx) =>
-
                             <div key={idx} className="list-in-card">
                                 <div >{item}</div>
-
-                                <div className="btn">x</div>
-
-
+                                <div className="btn" data-id={idx} onClick={this.removeListItem}>X</div>
                             </div>
-                        )
 
-
-                        : null}
-
-
-
-                    {/* <p>{this.props.content.type}</p> */}
+                        ) : null}
+                        <div className="space"></div>
                 </div>
 
-
-                {/* *********  CONTROL  ************ */}
+                {/* *********  CONTROL BAR: Color, Trash  ************ */}
                 <div className="control">
                     <div className="btn" onClick={this.deleteCard}><i className="far fa-trash-alt"></i></div>
-                    <div className="btn" onClick={this.openColorSelect}><i className="fas fa-palette"></i></div>
+                    {/* <div className="btn" onClick={this.openColorSelect}><i className="fas fa-palette"></i></div> */}
+                    <div className={`pick ${this.props.content.bg}`} onClick={this.openColorSelect}></div>
                 </div>
                 <div className={`colorModal ${this.state.colorModal}`}>
-
                     <div className="pick aaa" value="aaa" onClick={this.updateColor}></div>
                     <div className="pick bbb" value="bbb" onClick={this.updateColor}></div>
                     <div className="pick ccc" value="ccc" onClick={this.updateColor}></div>
                     <div className="pick ddd" value="ddd" onClick={this.updateColor}></div>
                     <div className="pick eee" value="eee" onClick={this.updateColor}></div>
-
                 </div>
-
             </div> // card
         )
     }
--- services/note-service.js
+++ services/note-service.js
@@ -9,27 +9,26 @@ export const noteService = {
 
 var notes = [];
 
-
 if (!Storage.loadFromStorage('notes') || Storage.loadFromStorage('notes') == '') { // if nothing in storage
   loadDump();
 } else {
   notes = Storage.loadFromStorage('notes');
 }
 
-function updateNotes(noteArr){
-  Storage.saveToStorage('notes',noteArr);
+function updateNotes(noteArr) {
+  Storage.saveToStorage('notes', noteArr);
   notes = noteArr;
 }
 
 function loadDump() {
   notes = [{
-    id: '54gg54',
-    title: 'Need to write a LOT of cards...',
-      body: 'Lets start...',
+      id: '54gg54',
+      title: 'Nevo\'s black list:',
+      body: '',
       bg: 'aaa',
       url: '',
       type: 'list',
-      list : ['item 1','item2'],
+      list: ['David Marom', 'Chen Edri','Another guy','and another'],
       video: ''
     }, {
       id: 's4gf54',
@@ -38,29 +37,29 @@ function loadDump() {
       bg: 'eee',
       url: '',
       type: 'text',
-      list : [],
+      list: [],
       video: ''
 
     },
-   {
+    {
       id: '55g454',
       title: 'Work on the project',
       body: 'Push commit etc..',
       bg: 'ccc',
       url: '',
       type: 'text',
-      list : [],
+      list: [],
       video: ''
 
     },
-  {
+    {
       id: '54sdfgf4y',
       title: 'Donate blood',
       body: 'Lie about the unprotected sex questions this time...',
       bg: 'ddd',
       url: '',
       type: 'text',
-      list : [],
+      list: [],
       video: ''
 
     }, {
@@ -70,7 +69,7 @@ function loadDump() {
       bg: 'aaa',
       url: 'https://media.giphy.com/media/kclGU7Fgztb9HtcHRI/giphy.gif',
       type: 'image',
-      list : [],
+      list: [],
       video: ''
 
     }, {
@@ -80,12 +79,11 @@ function loadDump() {
       bg: 'bbb',
       url: '',
       type: 'text',
-      list : [],
+      list: [],
       video: ''
 
     }
   ]
-
 
   Storage.saveToStorage('notes', notes);
 }
